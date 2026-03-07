09828a78074eeea5a977fee6ffbccfe8e7a3fb2d
raeyang <raeyangcoding@users.noreply.github.com>
2026-03-07 22:23:58 +0800

    Update

--- data/recommended-videos.js
+++ data/recommended-videos.js
@@ -1,7 +1,12 @@
-// 开发者推荐视频列表，可直接在此文件增删改
 window.RECOMMENDED_VIDEOS = [
-  { id: 'bl-1', source: 'bilibili', bvid: 'BV171421C7Cf', url: 'https://www.bilibili.com/video/BV171421C7Cf', title: '口风琴+气垫=？', cover: '' },
-  { id: 'bl-2', source: 'bilibili', bvid: 'BV1hRCvYNEg5', url: 'https://www.bilibili.com/video/BV1hRCvYNEg5', title: '谁说哈士奇不怕冷的？站出来，我家的你怎么解释！', cover: '' },
-  { id: 'yt-1', source: 'youtube', videoId: 'jCt_q8tzBp4', url: 'https://www.youtube.com/watch?v=jCt_q8tzBp4', title: "Elmo's Bedtime Routine | Sesame Street Songs and Stories Compilation", cover: 'https://img.youtube.com/vi/jCt_q8tzBp4/mqdefault.jpg' },
-  { id: 'yt-2', source: 'youtube', videoId: 'Aosze55r9OU', url: 'https://www.youtube.com/watch?v=Aosze55r9OU', title: "Mr Tumble's Bedtime Routine | Mr Tumble and Friends ", cover: 'https://img.youtube.com/vi/Aosze55r9OU/mqdefault.jpg' }
+    { title: "口风琴+气垫=？", url: "https://www.bilibili.com/video/BV171421C7Cf/" },
+    { title: "谁说哈士奇不怕冷的？站出来，我家的你怎么解释！", url: "https://www.bilibili.com/video/BV1hRCvYNEg5" },
+    { title: "【纪录/历史】德国史 2季（中德双字/附花絮）", url: "https://www.bilibili.com/video/BV1Lt411o7yn" },
+    { title: "Elmo's Bedtime Routine | Sesame Street Songs and Stories Compilation", url: "https://www.youtube.com/watch?v=jCt_q8tzBp4" },
+    { title: "Mr Tumble's Bedtime Routine | Mr Tumble and Friends", url: "https://www.youtube.com/watch?v=Aosze55r9OU" },
+    { title: "睡眠是我们的超能力！【TED演讲】", url: "https://www.bilibili.com/video/BV1Vb42177jo" },
+    { title: "【狐狸】在床上躺平任rua的涩涩狐狸 ©irisha.zai", url: "https://www.bilibili.com/video/BV1YD4y1W7NM" },
+    { title: "失眠必听丨助眠丨数学课催眠丨上课睡觉丨10分钟必睡着丨别再熬夜了！", url: "https://www.bilibili.com/video/BV1f54y1F7Az/" },
+    { title: "【中文配音/英语原音/中英双字】Headspace Guide to Sleep睡眠指南全7集，中英文双版本", url: "https://www.bilibili.com/video/BV1pg411V72N" },
+    { title: "《正念谋杀》正念纯享", url: "https://www.bilibili.com/video/BV1F3PTeWEvJ" }
 ];
